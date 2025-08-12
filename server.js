19e421c9c1854ec87f4b8a12897021c21920a279
Nugitha Disas <nugitha.c@gmail.com>
2025-08-12 19:22:46 +0530

    implement message sending functionality with socket.io integration and update imports

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 import express from 'express';
 import "dotenv/config";
-import http from 'http';
+import http  from 'http';
 import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
+import { log } from 'console';
 
 // create express app and http server
 const app = express();
 const server = http.createServer(app);
+
+//initialize socket.io
+export const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+// Store Online Users
+export const userSocketMap = {};
+
+//Socket.io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    log("User connected:", userId);
+
+    if (userId) userSocketMap[userId] = socket.id;
+
+    //emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
 
 // middleware
 app.use(express.json({ limit: '4mb' }));
