fa08d5c0acc7b1589b7ccc674caebb14c770dccf
Nugitha Disas <nugitha.c@gmail.com>
2025-08-15 21:29:30 +0530

    bug fix: correct user ID extraction in protectRoute middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,8 @@ export const protectRoute = async (req, res, next) => {
     try {
         const token = req.headers.token;
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.userId).select("-password");
+        const user = await User.findById(decoded.id).select("-password");
+        
         if (!user) return res.json({ success: false, message: "User not found" });
         req.user = user;
         next();
